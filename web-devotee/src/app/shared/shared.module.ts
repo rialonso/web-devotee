01780e774cc8589bc68527bd721d0f5037c6940d
muricardoso <muri.cardoso@hotmail.com>
2022-04-06 00:14:24 -0300

    criação de component para footer

---
+++ web-devotee/src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+  ],
+  imports: [
+
+  ],
+  exports:[
+    FooterComponent,
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class SharedModule { }
