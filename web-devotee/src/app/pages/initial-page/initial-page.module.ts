4621d03b0760d661378c59cfbef747b30bbbb8fb
muricardoso <muri.cardoso@hotmail.com>
2022-04-07 02:25:20 -0300

    ajustes de fontes

--- web-devotee/src/app/pages/initial-page/initial-page.module.ts
+++ web-devotee/src/app/pages/initial-page/initial-page.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InitialPageComponent } from './initial-page.component';
 import { InitialPageRoutingModule } from './initial-page-rounting.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     InitialPageRoutingModule,
+    SharedModule,
   ],
   declarations: [InitialPageComponent],
   exports: [
