f24ecb17d611874195f66106aa696eeec9b4f4e1
muricardoso <muricardoso@Murilos-MacBook-Pro.local>
2022-05-28 02:00:44 -0300

    inclusao de campos para cadastro

--- web-devotee/src/app/shared/shared.module.ts
+++ web-devotee/src/app/shared/shared.module.ts
@@ -4,43 +4,48 @@ import { NgModule } from '@angular/core';
 import { FooterComponent } from './components/footer/footer.component';
 import { IconModule } from './components/custom-icons/icon.module';
 import { HttpClientModule } from '@angular/common/http';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { DialogsModule } from './components/dialogs/dialogs.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HeaderCardsInitialPageComponent } from './components/header-cards-initial-page/header-cards-initial-page.component';
 import { CommonModule } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
 
 const components = [
   FooterComponent,
   HeaderCardsInitialPageComponent,
   CardImgTitleTextComponent,
-
+]
+const angularMaterialModules = [
+  MatInputModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatAutocompleteModule,
+  MatSelectModule,
+  MatCardModule,
 ]
 @NgModule({
   declarations: [
     ...components
   ],
   imports: [
+    ...angularMaterialModules,
     IconModule,
     HttpClientModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatIconModule,
     ReactiveFormsModule,
     CommonModule,
+
   ],
   exports: [
     ...components,
+    ...angularMaterialModules,
     IconModule,
     HttpClientModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatIconModule,
     ReactiveFormsModule,
     CommonModule,
-    MatCardModule,
   ],
   providers: [],
   bootstrap: []
