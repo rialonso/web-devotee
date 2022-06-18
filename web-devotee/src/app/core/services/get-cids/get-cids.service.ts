10bfdf769b4fcb5f804805aaf68a915b3f19cfb0
muricardoso <muricardoso@Murilos-MacBook-Pro.local>
2022-06-18 13:35:09 -0300

    remove imports not used in services

--- web-devotee/src/app/core/services/get-cids/get-cids.service.ts
+++ web-devotee/src/app/core/services/get-cids/get-cids.service.ts
@@ -3,7 +3,6 @@ import { HttpService } from '../generics-http/httpService.service';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { ModelHosptals } from 'src/app/shared/model/response/get-hosptals/get-hosptals.model';
 import { ModelCidsResponse } from 'src/app/shared/model/response/get-cids/get-cids.model';
 
 @Injectable({
--- web-devotee/src/app/core/services/get-medical-procedures/get-medical-procedures.service.ts
+++ web-devotee/src/app/core/services/get-medical-procedures/get-medical-procedures.service.ts
@@ -2,7 +2,6 @@ import { HttpService } from './../generics-http/httpService.service';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { ModelGetMatchesResponse } from 'src/app/shared/model/response/get-matches/get-matches.response';
 
 @Injectable({
   providedIn: 'root'
