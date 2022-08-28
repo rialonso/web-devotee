f2e53abe74e61cd15111418342353303a1749abf
muricardoso <muricardoso@Murilos-MacBook-Pro.local>
2022-08-28 02:29:05 -0300

    open login page

--- web-devotee/src/app/pages/landing-page/landing-page.component.ts
+++ web-devotee/src/app/pages/landing-page/landing-page.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { TranslateService } from 'src/app/core/services/translate/translate.service';
 import { EnumRoutesApplication } from 'src/app/shared/enum/routes.enum';
 import { RouteService } from 'src/app/shared/functions/routes/route.service';
+import { IAppState } from 'src/app/state-management/app.model';
+import { AddControlApp } from 'src/app/state-management/controls/copntrols-app.action';
 
 @Component({
   selector: 'app-landing-page',
@@ -12,8 +15,9 @@ export class LandingPageComponent implements OnInit {
   dataTexts;
   routesApp = EnumRoutesApplication;
   constructor(
+    protected store: Store<IAppState>,
     private translateService: TranslateService,
-    private routeService: RouteService
+    private routeService: RouteService,
   ) {
     this.dataTexts = this.translateService?.textTranslate;
   }
@@ -22,5 +26,7 @@ export class LandingPageComponent implements OnInit {
   }
   navigateTo(route: EnumRoutesApplication) {
     this.routeService.navigateToURL(route);
+    // this.store.dispatch(new AddControlApp({ openSingIn: true }));
+
   }
 }
