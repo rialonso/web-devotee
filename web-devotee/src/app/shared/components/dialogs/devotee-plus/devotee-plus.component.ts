08e35caebb2254255f568265609d3ca0f703bbdc
muricardoso <muricardoso@Murilos-MacBook-Pro.local>
2022-08-30 00:04:35 -0300

    create dialog to devotee plus

---
+++ web-devotee/src/app/shared/components/dialogs/devotee-plus/devotee-plus.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-devotee-plus',
+  templateUrl: './devotee-plus.component.html',
+  styleUrls: ['./devotee-plus.component.scss']
+})
+export class DevoteePlusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
