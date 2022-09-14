6bf6de5a1576ddbace8f47b02608bf57414c85f2
Murilo Cardoso <muri.cardoso@hotmail.com>
2022-09-14 00:04:25 -0300

    finish page likes

--- web-devotee/src/app/pages/liked-me/liked-me.component.ts
+++ web-devotee/src/app/pages/liked-me/liked-me.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { State } from '@ngrx/store';
 import { LikedMeService } from 'src/app/core/services/liked-me/liked-me.service';
 import { TranslateService } from 'src/app/core/services/translate/translate.service';
 import { UserProfileService } from 'src/app/core/services/user-profile/user-profile.service';
 import { DialogsService } from 'src/app/shared/functions/dialogs/dialogs.service';
 import { TransformAgeService } from 'src/app/shared/functions/transform-age/transform-age.service';
 import { ILikedMe } from 'src/app/shared/model/response/get-liked-me/get-liked-me.model';
+import { IAppState } from 'src/app/state-management/app.model';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -14,8 +16,12 @@ import { environment } from 'src/environments/environment';
 })
 export class LikedMeComponent implements OnInit {
   dataTexts
-  userLikeds: ILikedMe.RootObject
+  userLikeds: ILikedMe.RootObject;
+  loading = false;
+
+  userPlanType: string;
   constructor(
+    protected state: State<IAppState>,
     private translateService: TranslateService,
     private likedMeService: LikedMeService,
     private transformAgeService: TransformAgeService,
@@ -27,13 +33,14 @@ export class LikedMeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.userPlanType = this.state.getValue().userData.data.plan_type;
     this.getLikedMe();
   }
   private async getLikedMe() {
+    this.loading = true;
     const likedMeData = await this.likedMeService.get().toPromise();
     this.userLikeds = likedMeData.data;
-    console.log(this.userLikeds);
-
+    this.loading = false;
   }
   changeUserImageInBackground(profilePicture): string {
     return `background-image: url(${environment.urlImages}${profilePicture[0]?.path});
@@ -54,8 +61,6 @@ export class LikedMeComponent implements OnInit {
     const diffInMinutes = diffInMs / (1000 * 60);
 
     if (dateLiked.getDate() === nowDate.getDate()) {
-      console.log(diffInHours);
-
       if (Math.abs(diffInHours) < 1) {
         if (Math.abs(diffInMinutes) == 1 ) {
           return `
@@ -87,9 +92,12 @@ export class LikedMeComponent implements OnInit {
     }
   }
   async openProfile(userId) {
-    // this.loading = true;
-    const userData = await this.userProfileService.get(userId).toPromise();
-    // this.loading = false;
-    this.dialogsService.openProfile(userData.data);
+    if (this.userPlanType === 'free') {
+
+    } else {
+      const userData = await this.userProfileService.get(userId).toPromise();
+      this.dialogsService.openProfile(userData.data);
+    }
+
   }
 }
