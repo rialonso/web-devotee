d8d8ce896f09444232b22c3b93adb49222542e59
Murilo Cardoso <muri.cardoso@hotmail.com>
2023-05-09 09:38:16 -0300

    unit test http clients

--- web-devotee/src/app/core/services/generate-hash-qrcode/generate-hash-qrcode.service.spec.ts
+++ web-devotee/src/app/core/services/generate-hash-qrcode/generate-hash-qrcode.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GenerateHashQrcodeService } from './generate-hash-qrcode.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('GenerateHashQrcodeService', () => {
   let service: GenerateHashQrcodeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(GenerateHashQrcodeService);
   });
 
